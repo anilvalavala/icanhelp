80579731404931ed13debf1314c3812ebcbe15b8
Anil Valavala <anilkumar.v@hotmail.com>
2018-06-09 19:35:54 +0530

    Included configurations related to Heroku

--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
+//Heroku is going to set the port number dynamically, hence access corresponding port
+//Else in case of running express locally use 3000
+const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
@@ -11,6 +14,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server is up!');
 })
